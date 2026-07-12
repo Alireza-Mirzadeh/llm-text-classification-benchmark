bb1f06d5d043e21e4a9983469fb0ef6ee5372c7e
Alireza-Mirzadeh <alirezamirzadeh313@gmail.com>
2026-07-12 14:11:16 +0200

    Add Transformer sentiment model results

--- src/models/hf_pipeline_classifier.py
+++ src/models/hf_pipeline_classifier.py
@@ -1,14 +1,24 @@
-""" Hugging Face Pipeline Classifier """
+""" Hugging Face Pipeline Classifier for Taslk-Specific Transformer Sentiment Model """
 
 import logging
 import numpy as np
 import torch
 from transformers import pipeline
 from tqdm.auto import tqdm
+from transformers.pipelines.pt_utils import KeyDataset
 from sklearn.base import BaseEstimator, ClassifierMixin
 
 class HuggingFacePipelineClassifier(BaseEstimator, ClassifierMixin):
-    """ Hugging Face Pipeline Classifier """
+    """ Hugging Face Pipeline Classifier for Task-Specific Transformer Sentiment Model
+     This class wraps a Hugging Face pipeline for text classification, allowing it to be used as a scikit-learn compatible classifier.
+     
+     Args:
+        BaseEstimator: Base class for all estimators in scikit-learn.
+        ClassifierMixin: Mixin class for all classifiers in scikit-learn.
+    
+    Returns:
+        A scikit-learn compatible classifier that uses a Hugging Face pipeline for text classification.
+    """
 
     def __init__(self, model_name: str, batch_size: int = 32, device = "auto"):
         """ 
@@ -30,7 +40,10 @@ class HuggingFacePipelineClassifier(BaseEstimator, ClassifierMixin):
         self.pipe = None
 
     def _get_device(self):
-        """ Determine the device to use for inference."""
+        """ Determine the device to use for inference. 
+        Returns:
+            int: The device index to use for the Hugging Face pipeline. Returns 0 for GPU if available, -1 for CPU, or the specified device index.
+        """
 
         # If the device is set to "auto", check if CUDA is available and return the appropriate device index.
         if self.device == "auto":
@@ -44,36 +57,47 @@ class HuggingFacePipelineClassifier(BaseEstimator, ClassifierMixin):
         return self.device
 
     def fit (self, X=None, y=None):
-        """ Load  the Hugging Face pipeline for the specified model. No training is performed as the model is pre-trained. """
+        """ Load  the Hugging Face pipeline for the specified model. No training is performed as the model is pre-trained. 
+         Args:
+            X: Ignored. Present for API consistency by convention.
+            y: Ignored. Present for API consistency by convention.
+            
+        Returns:
+            self: Returns the instance itself.
+        """
 
         self.logger.info(f"Loading Hugging Face pipeline for model: {self.model_name}")
 
+        # Initialize the Hugging Face pipeline for text classification with the specified model and tokenizer. The device is determined based on the availability of CUDA or the specified device index.
         self.pipe = pipeline(
             task="text-classification",
             model=self.model_name,
             tokenizer=self.model_name,
-            return_all_scores=True,
+            top_k=None,
             device=self._get_device()
         )
 
         return self
 
-    def predict(self, X):
+    # Predict method to classify the input dataset using the Hugging Face pipeline.
+    def predict(self, dataset, verbose=False):
         """ Predict the class labels for the input data X using the Hugging Face pipeline. """
 
         predictions = []
 
-        # Use the pipeline to get predictions for the input data X in batches
-        outputs = self.pipe(X, batch_size=self.batch_size, truncation=True)
+        # Process the input data in batches and obtain predictions from the pipeline.
+        for output in tqdm(self.pipe(KeyDataset(dataset, "text"), batch_size=self.batch_size), desc="Predicting", total=len(dataset)):
+            if verbose:
+                print(f"Output: {output}")
+            scores = {item["label"].lower(): item["score"] for item in output}
+            if verbose:
+                print(f"Scores: {scores}")
+            # Determine the predicted class based on the scores.
+            prediction = int(scores["positive"] > scores["negative"])
+            if verbose:
+                print(f"Prediction: {prediction} \n")
+            predictions.append(prediction)
 
-        # Iterate through the outputs and extract the predicted class labels based on the scores
-        for output in outputs:
-
-            negative_score = output[0]['score']
-            positive_score = output[1]['score']
-
-            # Append the predicted class label (0 for negative, 1 for positive) based on the scores
-            predictions.append(np.argmax([negative_score, positive_score]))
-
+            
         # Return the predictions as a NumPy array
         return np.array(predictions)
